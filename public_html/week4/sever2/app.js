dfd6f051142ef4d14c815a72f595e6a45807382a
ctomas@email.neit.edu <ctomas@email.neit.edu>
2021-02-02 19:25:16 -0500

    assigment 3

--- public_html/week4/sever2/app.js
+++ public_html/week4/sever2/app.js
@@ -5,9 +5,13 @@ var app = express()
 var randNum = require('./modules/randomNum');
 app.set('view engine', 'hbs')
 hbs.registerHelper('ptag',function(num){
-    var msq = `<p>${num}</p>`
+    var msg = `<p>${num}</p>`
+    return new hbs.handlebars.SafeString(msg)
 })
 app.get('/form',function(req,res){
+    res.render('form.hbs')
+})
+app.get('/results',function(req,res){
     res.render('form.hbs')
 })
 console.log(randNum.rando());
